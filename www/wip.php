89af43fe456fd6f64408f17b32239fc72f2a21e0
robflop <robflop98@web.de>
2016-05-14 23:30:32 +0200

    Counter save working! (No display yet though)

--- www/wip.php
+++ www/wip.php
@@ -6,12 +6,14 @@
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="css/style.css">
         <script src="js/count.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
     </head>
     <body>
         <div id="box">
-		  <a id="counter">0</a>
-		  <button id="button" onclick="play()">やめろ!!</button>
+		  <div id="counter">0</div>
+            <button id="button" onclick="count();">やめろ!!</button>
         </div>
         <audio id="yamero" src="yamero.mp3" preload="auto"></audio>
+    
 	</body>
 </html>
