40395b9d9c248ae1302856455d0a648c7e27d2bd
robflop <robflop98@gmail.com>
2016-05-16 22:00:09 +0200

    Credits page implemented

--- proto2.php
+++ proto2.php
@@ -5,9 +5,10 @@
         <title>Megumin is love</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="css/style.css">
+        <link href='https://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
         <script src="js/count_proto2.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/ion-sound/3.0.7/js/ion.sound.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-sound/3.0.7/js/ion.sound.min.js"></script>
     </head>
     <body>
         <?php $countfile = fopen("counter.txt", "r")?>
@@ -18,4 +19,7 @@
         </div>
         <audio id="yamero" src="yamero.mp3" preload="auto"></audio>
 	</body>
+	<footer>
+        <p>Created and maintained by robflop<a id="credits" href="credits.html"> -> Credits</a></p>
+	</footer>
 </html>
