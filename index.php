68963c2a72f2827f68e4b302269e2c6889c56211
robflop <robflop98@web.de>
2016-05-24 15:22:35 +0200

    (More adjustments)

--- index.php
+++ index.php
@@ -25,12 +25,12 @@
 </head>
 
 <body>
-    <div id="box" style="display:none;">
+    <div id="box">
         <div id="counter">
             <?php file_get_contents("includes/update_counter.php");?>
         </div>
         <button id="button">やめろ!!</button>
-        <a href="version.html" id="version">[ver. P2]</a>
+        <a href="version.html" id="version">[ver. P3]</a>
         <span id="share-buttons">
             <a href="https://twitter.com/intent/tweet?text=New%20Megumin%20Fansite%21%20Check%20it%20out%21&via=robflop98&url=https%3A%2F%2Fmegumin.love" onclick="window.open(this.href, '', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes'); return false;">
             <img src="/images/twitter.png" alt="Tweet on Twitter" /></a>
