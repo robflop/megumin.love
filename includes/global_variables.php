b7498e11108de640bdd8f3568d835c559b186e1c
robflop <robflop98@gmail.com>
2016-05-24 15:13:05 +0200

    Update global_variables.php

--- includes/global_variables.php
+++ includes/global_variables.php
@@ -1,10 +1,6 @@
 <?php
-/* 
-* I suggest to keep files like this (and all PHP files that don't require ajax or a direct connection to the client) 
-* outside of the webroot (for example, in the folder above public_html). Easier to manage, and will keep these more secure. 
-*/
-define('HOST', 'localhost'); //Designated URL for your SQL server
-define('USERNAME', 'user'); //Username of...user
-define('PASSWORD', 'password'); //Password of aformentioned user
-define('DATABASE', 'database_name'); //Name of the database that contains the table 'megumin_love_counter'
+define('HOST', 'localhost');
+define('USERNAME', 'user');
+define('PASSWORD', 'password');
+define('DATABASE', 'database_name');
 ?>
