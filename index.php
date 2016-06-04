c504521a31037dc53a3d1fa45e864684f762d69b
robflop <robflop98@gmail.com>
2016-06-04 15:33:54 +0200

    Version 1.0! Woo!
    It's time boys!

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $currentCount = ob_get_clean();
                 <?php echo $currentCount; ?>
             </div>
             <button id="button">やめろ!!</button>
-            <a href="version.html" id="version">[ver. P3]</a>
+            <a href="version.html" id="version">[ver1.0]</a>
             <span id="share-buttons">
             <a href="https://twitter.com/intent/tweet?text=New%20Megumin%20Fansite%21%20Check%20it%20out%21&via=robflop98&url=https%3A%2F%2Fmegumin.love" onclick="window.open(this.href, '', 'width=650, height=450, menubar=no, toolbar=no, scrollbars=yes'); return false;">
             <img src="/images/twitter.png" alt="Tweet on Twitter" /></a>
