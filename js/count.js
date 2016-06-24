2281f63c5078393533b040c1726d1cf3bbce670e
robflop <robflop98@gmail.com>
2016-06-24 06:14:45 +0200

    More sound formats + sounds folder
    (Also a cool "contact me" phrase)

--- js/count.js
+++ js/count.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         sounds: [{
             name: "yamero"
         }],
-        path: "./",
+        path: "sounds/",
         preload: true,
         multiplay: true,
     });
