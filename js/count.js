5a65fc1457dca0c039bdbf68141b71b38e166eef
robflop <robflop98@gmail.com>
2016-06-28 13:21:42 +0200

    Sound randomization (+removal of unnecessary file formats)

--- js/count.js
+++ js/count.js
@@ -9,15 +9,36 @@ $(document).ready(function () {
     $('#box').fadeIn(250);
     console.log("No stealing code! ;) -- Check out the Github Repo at https://git.io/vrrEi instead.");
     ion.sound({
-        sounds: [{
-            name: "yamero"
-        }],
+        sounds: [
+            {name: "yamero"},
+            {name: "pull"},
+            {name: "explosion"},
+            {name: "itai"},
+            {name: "name"},
+        ],
         path: "sounds/",
         preload: true,
         multiplay: true,
     });
     $('#button').click(function () {
-        ion.sound.play("yamero");
+        var rsound = Math.floor(Math.random() * 5);
+        switch(rsound) {
+            case 0:
+                ion.sound.play("yamero");
+                break;
+            case 1:
+                ion.sound.play("pull");
+                break;
+            case 2:
+                ion.sound.play("explosion");
+                break;
+            case 3:
+                ion.sound.play("itai");
+                break;
+            case 4:
+                ion.sound.play("name");
+                break;
+        }
         var curr =
             $('#counter').html(parseInt($('#counter').html()) + 1);
         $.ajax({
