d17e98b7118c368970f64b861c2b91db32fb6ebb
robflop <robflop98@gmail.com>
2016-06-30 12:32:07 +0200

    Fixed sharing previews (twitter cards, fb preview etc)

--- index.php
+++ index.php
@@ -9,21 +9,24 @@ $currentCount = ob_get_clean();
     <head>
         <meta charset="utf-8">
         <!--FACEBOOK-->
-        <meta property="og:title" content="Megumin is love">
-        <meta property="og:site_name" content="Megumin is love">
+        <meta property="og:title" content="Megumin is love!">
+        <meta property="og:site_name" content="Megumin is love!">
         <meta property="og:url" content="https://megumin.love">
         <meta property="og:description" content="Fansite about best girl Megumin from the anime called 'Kono Subarashii Sekai ni Shukufuku wo!'">
         <meta property="og:type" content="website">
         <meta property="og:locale" content="en_US">
+        <meta property="og:image" content="https://megumin.love/images/bg.png">
         <!--TWITTER-->
         <meta property="twitter:card" content="summary">
-        <meta property="twitter:title" content="Megumin is love">
+        <meta property="twitter:title" content="Megumin is love!">
         <meta property="twitter:description" content="Fansite about best girl Megumin from the anime called 'Kono Subarashii Sekai ni Shukufuku wo!'">
         <meta property="twitter:creator" content="@robflop98">
         <meta property="twitter:url" content="https://megumin.love">
+        <meta property="twitter:site" content="@robflop98">
+        <meta property="twitter:image" content="https://megumin.love/images/bg.png">
         <meta name="keywords" content="megumin, love, konosuba, best girl, best, girl, anime">
         <meta name="description" content="Fansite about best girl Megumin from the anime called 'Kono Subarashii Sekai ni Shukufuku wo!'">
-        <title>Megumin is love</title>
+        <title>Megumin is love!</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="css/style.css">
         <link href='https://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
