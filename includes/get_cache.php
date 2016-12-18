b29407efe85ffaa8bc440ebd9e6d4e5842c596cc
robflop <robflop98@web.de>
2016-12-18 02:44:01 +0100

    Global self-updating counter!
    The counter now updates on an interval, updating to the most-recent
    number for anyone currently viewing the site.
    
    Never refresh the page again just to see if anyone else besides you has
    clicked!

---
+++ includes/get_cache.php
@@ -0,0 +1,16 @@
+<?php
+include('global_variables.php'); // Load necessary ressources and make connections
+$cacheCounter = new Memcached();
+$cacheCounter->addServer(MEMCACHED_HOST, MEMCACHED_PORT) or die("Memcached connection failed!");
+
+ // Check if memcached is running to prevent db corruption
+if(!$cacheCounter->addServer(MEMCACHED_HOST, MEMCACHED_PORT)) {
+    printf("Memcached server not running or connection failed, update aborted");
+    die();
+}
+
+if(isset($_GET['update']) && $_GET['update'] == 1) { 
+	$cacheNumber = $cacheCounter->get('yamero_counter');
+	echo $cacheNumber;
+}
+?>
