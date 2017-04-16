c9b001d28fd2c75391b9096c6b1ffb10fb7ee89d
robflop <robflop98@gmail.com>
2017-04-16 09:22:47 +0200

    arrow functions break things, apparently

--- resources/js/soundboard.js
+++ resources/js/soundboard.js
@@ -1,8 +1,8 @@
-$(document).ready(() => {
+$(document).ready(function() {
 	console.log("No stealing code! ;) -- Check out the Github Repo at https://git.io/vrrEi instead.");
 	var howlerList = {};
 
-	$('#name').click(() => {
+	$('#name').click(function() {
 		var rsound = Math.floor(Math.random() * 100) + 1;
 		switch(rsound) {
 		case 42:
@@ -18,7 +18,7 @@ $(document).ready(() => {
 	for(var i = sounds.length - 1; i >= 0; i--) {
 		howlerList[sounds[i]] = new Howl({src: ["/sounds/"+sounds[i]+".mp3", "/sounds/"+sounds[i]+".ogg", "/sounds/"+sounds[i]+".aac"]});
 		if(sounds[i] == "name") continue;
-		$("#"+sounds[i]).click(() => {
+		$("#"+sounds[i]).click(function() {
 			howlerList[$(this).attr("id")].play();
 		});
 	};
