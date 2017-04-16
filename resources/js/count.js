bca5c73c45d77798a019d6e651989f9d568197c5
robflop <robflop98@web.de>
2017-04-16 09:19:38 +0200

    route changes + arrow functions

--- resources/js/count.js
+++ resources/js/count.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(() => {
 	console.log("No stealing code! ;) -- Check out the Github Repo at https://git.io/vrrEi instead.");
 	howlerList = {};
 	sounds.indexOf('realname')>-1?sounds.splice(sounds.indexOf('realname'),1):"";
@@ -6,18 +6,18 @@ $(document).ready(function() {
 		howlerList[sounds[i]] = new Howl({src: ["/sounds/"+sounds[i]+".mp3", "/sounds/"+sounds[i]+".ogg", "/sounds/"+sounds[i]+".aac"]});
 		// load all sounds
 	};
-	$('#button').keypress(function(key) {
+	$('#button').keypress((key) => {
 		if(key.which == 13) {
 			key.preventDefault(); // don't trigger the button on "enter" keypress
 		};
 	});
-	$('#button').click(function() { // Randomize sound that is played on buttonclick
+	$('#button').click(() => {
 		var sound = sounds[Math.floor(Math.random()*sounds.length)];
+		// Randomize sound that is played on buttonclick
 		howlerList[sound].play();
 		$('#counter').html(parseInt($('#counter').html()) + 1);
 		// Increase counter client-side
-		// $.get("https://megumin.love/increment").done(function(res) {});
-		$.get("/increment").done(function(res) {});
+		$.get("/counter?inc=1").done((res)=>{});
         // Send GET-request to increase counter server-side
 	});
 });
