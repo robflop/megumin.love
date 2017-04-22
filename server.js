e8d654179e013ae9da2e186c7d76a42d7f99b66d
robflop <robflop98@web.de>
2017-04-23 01:33:07 +0200

    fix upsert query the third

--- server.js
+++ server.js
@@ -106,7 +106,8 @@ scheduler.scheduleJob(`*/${Math.round(config.updateInterval)} * * * *`, () => {
 	timestamp = moment().format('DD/MM/YYYY HH:mm:ss');
 	db.serialize(() => {
 		db.run("UPDATE yamero_counter SET `counter` ="+counter);
-		db.run(`UPDATE statistics SET count = ${today} WHERE date = date('now', 'localtime'); INSERT OR IGNORE INTO statistics ( date, count ) VALUES ( ${todayDate}, ${today} );`);
+		db.run(`INSERT OR IGNORE INTO statistics ( date, count ) VALUES ( date('now', 'localtime'), ${today} )`);
+		db.run(`UPDATE statistics SET count = ${today} WHERE date = date('now', 'localtime')`);
 		console.log(`[${timestamp}] Database updated.`);
 	});
 }); // update db at the configured minute of each hour
