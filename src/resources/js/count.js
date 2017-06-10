806f1bd5fa9487aaa9e3f419a8475863d1250ffa
robflop <robflop98@web.de>
2017-06-10 14:42:31 +0200

    Optimization

--- src/resources/js/count.js
+++ src/resources/js/count.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
 	const formatNumber = number => number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');
 
 	const domainOrIP = document.URL.split('/')[2].split(':')[0];
+	const howlerList = {};
 	let host = `http://${domainOrIP}`;
 	let socket;
 
@@ -10,26 +11,21 @@ $(document).ready(() => {
 		host = SSLproxy ? host.replace('http', 'https') : host += `:${port}`;
 
 		socket = io.connect(host);
-		socket.on('update', data => {
-			$('#counter').html(formatNumber(data.counter));
-		});
+		socket.on('update', data => $('#counter').html(formatNumber(data.counter)));
 	});
 
 	$.get('/counter').done(res => $('#counter').html(formatNumber(res)));
 	// load initial counter
 
-	const howlerList = {};
-	sounds.indexOf('realname') > -1 ? sounds.splice(sounds.indexOf('realname'), 1) : '';
-	for (let i = sounds.length - 1; i >= 0; i--) {
-		howlerList[sounds[i]] = new Howl({ src: [`/sounds/${sounds[i]}.mp3`, `/sounds/${sounds[i]}.ogg`, `/sounds/${sounds[i]}.aac`] });
+	sounds.includes('realname') ? sounds.splice(sounds.indexOf('realname'), 1) : null;
+	for (const sound of sounds) {
+		howlerList[sound] = new Howl({ src: [`/sounds/${sound}.mp3`, `/sounds/${sound}.ogg`, `/sounds/${sound}.aac`] });
 		// load all sounds
 	}
 
 	$('#button').keypress(key => {
-		if (key.which === 13) {
-			return key.preventDefault();
-			// don't trigger the button on 'enter' keypress
-		}
+		if (key.which === 13) return key.preventDefault();
+		// don't trigger the button on 'enter' keypress
 	});
 
 	$('#button').click(() => {
