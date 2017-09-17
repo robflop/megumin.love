2ff1b1ba366310b6ad02fb73c7e77066779873d2
robflop <robflop98@gmail.com>
2017-09-17 22:19:38 +0200

    move socket stuff in appropriate scope

--- src/resources/js/statistics.js
+++ src/resources/js/statistics.js
@@ -1,15 +1,12 @@
 $(document).ready(() => {
 	const formatNumber = number => number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');
 
-	const domainOrIP = document.URL.split('/')[2].split(':')[0];
-	let host = `http://${domainOrIP}`;
-	let socket;
-
 	$.get('/conInfo').done(con => {
+		const domainOrIP = document.URL.split('/')[2].split(':')[0];
 		const port = con.port, SSLproxy = con.ssl;
-		host = SSLproxy ? host.replace('http', 'https') : host += `:${port}`;
+		const host = SSLproxy ? `https://${domainOrIP}` : `http://${domainOrIP}:${port}`;
+		const socket = io.connect(host);
 
-		socket = io.connect(host);
 		socket.on('update', data => {
 			$('#alltime').html(`All-time clicks: ${formatNumber(data.statistics.alltime)}`);
 			$('#today').html(`Today's clicks: ${formatNumber(data.statistics.today)}`);
