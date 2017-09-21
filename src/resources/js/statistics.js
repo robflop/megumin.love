db4a254ac817e6840997fdf0a8eb7725f278ca67
robflop <robflop98@gmail.com>
2017-09-21 20:01:53 +0200

    Less code repetition (make function)

--- src/resources/js/statistics.js
+++ src/resources/js/statistics.js
@@ -1,26 +1,21 @@
 $(document).ready(() => {
 	const formatNumber = number => number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');
 
+	const updateStatistics = statistics => {
+		$('#alltime').html(`All-time clicks: ${formatNumber(statistics.alltime)}`);
+		$('#today').html(`Today's clicks: ${formatNumber(statistics.daily)}`);
+		$('#week').html(`This week's clicks: ${formatNumber(statistics.weekly)}`);
+		$('#month').html(`This month's clicks: ${formatNumber(statistics.monthly)}`);
+		$('#average').html(`Average clicks a day (in this month): ~${formatNumber(statistics.average)}`);
+	};
+
 	$.get('/conInfo').done(con => {
 		const domainOrIP = document.URL.split('/')[2].split(':')[0];
-		const port = con.port, SSLproxy = con.ssl;
-		const host = SSLproxy ? `https://${domainOrIP}` : `http://${domainOrIP}:${port}`;
+		const host = con.ssl ? `https://${domainOrIP}` : `http://${domainOrIP}:${con.port}`;
+
 		const socket = io.connect(host);
-
-		socket.on('update', data => {
-			$('#alltime').html(`All-time clicks: ${formatNumber(data.statistics.alltime)}`);
-			$('#today').html(`Today's clicks: ${formatNumber(data.statistics.today)}`);
-			$('#week').html(`This week's clicks: ${formatNumber(data.statistics.week)}`);
-			$('#month').html(`This month's clicks: ${formatNumber(data.statistics.month)}`);
-			$('#average').html(`Average clicks a day (in this month): ~${formatNumber(data.statistics.average)}`);
-		});
+		socket.on('update', data => updateStatistics(data.statistics));
 	});
 
-	$.get('/counter?statistics').done(statistics => {
-		$('#alltime').html(`All-time clicks: ${formatNumber(statistics.alltime)}`);
-		$('#today').html(`Today's clicks: ${formatNumber(statistics.today)}`);
-		$('#week').html(`This week's clicks: ${formatNumber(statistics.week)}`);
-		$('#month').html(`This month's clicks: ${formatNumber(statistics.month)}`);
-		$('#average').html(`Average clicks a day (in this month): ~${formatNumber(statistics.average)}`);
-	});
+	$.get('/counter?statistics').done(statistics => updateStatistics(statistics));
 });
