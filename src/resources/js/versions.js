62ad0ce09dc2a0edbfacfd5a3161a3e8705b3f16
robflop <robflop98@gmail.com>
2017-11-30 21:35:27 +0100

    Specify refresh for bg change
    How the hell did I forget this page??

---
+++ src/resources/js/versions.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+	// background
+
+	const backgroundCookie = (document.cookie.split(';').find(cookie => cookie.trim().startsWith('background')) || '').trim();
+	const background = backgroundCookie.substr(backgroundCookie.indexOf('=') + 1);
+
+	if (background === 'rotate' || !background) {
+		// falsy check for if no selection has been made, and thus is on standard
+		$('body').css('background-image', `url(/images/${['bg1.jpg', 'bg2.jpg', 'bg3.jpg'][Math.round(Math.random() * 2)]})`);
+	}
+	else {
+		$('body').css('background-image', `url(/images/${background}.jpg)`);
+	}
+
+	$('#bg-select').val(background || 'rotate');
+
+	$('#bg-select').change(function() {
+		/* eslint-disable no-invalid-this */
+		if (this.value !== 'rotate') $('body').css('background-image', `url(/images/${this.value}.jpg)`);
+		return document.cookie = `background=${this.value}`;
+		/* eslint-enable no-invalid-this */
+	});
+});
