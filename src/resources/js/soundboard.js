42387c98d9c1f33aac678d6c65dacdff3427ba4d
robflop <robflop98@gmail.com>
2017-12-03 02:34:44 +0100

    Fix soundboard websocket

--- src/resources/js/soundboard.js
+++ src/resources/js/soundboard.js
@@ -31,63 +31,63 @@ $(document).ready(() => {
 		const host = con.ssl ? `wss://${domainOrIP}` : `ws://${domainOrIP}:${con.port}`;
 
 		const ws = new WebSocket(host);
-	});
 
-	$('#container').append('<a href="/" id="backlink-top" class="backlink">Back</a>');
-	$('#container').append('<a href="rankings" id="rankings">Rankings</a>');
+		$('#container').append('<a href="/" id="backlink-top" class="backlink">Back</a>');
+		$('#container').append('<a href="rankings" id="rankings">Rankings</a>');
 
-	// Create buttons and make them play corresponding sounds
-	for (const sound of sounds) {
-		const source = $(`div.buttons-wrap.source-${sound.source.replace(/\s/g, '-').toLowerCase()}`);
+		// Create buttons and make them play corresponding sounds
+		for (const sound of sounds) {
+			const source = $(`div.buttons-wrap.source-${sound.source.replace(/\s/g, '-').toLowerCase()}`);
 
-		howlerList[sound.filename] = new Howl({
-			src: [`/sounds/${sound.filename}.ogg`, `/sounds/${sound.filename}.mp3`],
-		});
-
-		if (sound.filename === 'realname') continue;
-		// don't create button for this one
-
-		if (source.length) {
-			source.append(`<button id=${sound.filename}>${sound.displayName}</button>`);
-		}
-		else {
-			$('#container').append(`<div class="titles">${sound.source}:</div>`);
-			$(`<div class="buttons-wrap source-${sound.source.replace(/\s/g, '-').toLowerCase()}">`)
-				.appendTo('#container')
-				.append(`<button id=${sound.filename}>${sound.displayName}</button>`);
-			// use appendTo to get reference to newly-created wrapper in return value which is then appended to
-		}
-
-		if (sound.filename === 'name') {
-			$('#name').click(() => {
-				const rsound = Math.floor(Math.random() * 100) + 1;
-
-				if (rsound === 42) {
-					howlerList.realname.play();
-					ws.send(JSON.stringify({ type: 'sbClick', sound: sounds.find(s => s.filename === 'realname') }));
-				}
-				else {
-					howlerList.name.play();
-					ws.send(JSON.stringify({ type: 'sbClick', sound: sounds.find(s => s.filename === 'name') }));
-				}
+			howlerList[sound.filename] = new Howl({
+				src: [`/sounds/${sound.filename}.ogg`, `/sounds/${sound.filename}.mp3`],
 			});
 
-			continue;
-			// create button but don't use standard click function
+			if (sound.filename === 'realname') continue;
+			// don't create button for this one
+
+			if (source.length) {
+				source.append(`<button id=${sound.filename}>${sound.displayName}</button>`);
+			}
+			else {
+				$('#container').append(`<div class="titles">${sound.source}:</div>`);
+				$(`<div class="buttons-wrap source-${sound.source.replace(/\s/g, '-').toLowerCase()}">`)
+					.appendTo('#container')
+					.append(`<button id=${sound.filename}>${sound.displayName}</button>`);
+				// use appendTo to get reference to newly-created wrapper in return value which is then appended to
+			}
+
+			if (sound.filename === 'name') {
+				$('#name').click(() => {
+					const rsound = Math.floor(Math.random() * 100) + 1;
+
+					if (rsound === 42) {
+						howlerList.realname.play();
+						ws.send(JSON.stringify({ type: 'sbClick', sound: sounds.find(s => s.filename === 'realname') }));
+					}
+					else {
+						howlerList.name.play();
+						ws.send(JSON.stringify({ type: 'sbClick', sound: sounds.find(s => s.filename === 'name') }));
+					}
+				});
+
+				continue;
+				// create button but don't use standard click function
+			}
+
+			$(`#${sound.filename}`).click(() => {
+				howlerList[sound.filename].play();
+
+				ws.send(JSON.stringify({ type: 'sbClick', sound: sounds.find(s => s.filename === sound.filename) }));
+			});
+
+			$(`#${sound.filename}`).keypress(key => {
+				if (key.which === 13) return key.preventDefault();
+				// don't trigger the button on 'enter' keypress
+			});
 		}
 
-		$(`#${sound.filename}`).click(() => {
-			howlerList[sound.filename].play();
-
-			ws.send(JSON.stringify({ type: 'sbClick', sound: sounds.find(s => s.filename === sound.filename) }));
-		});
-
-		$(`#${sound.filename}`).keypress(key => {
-			if (key.which === 13) return key.preventDefault();
-			// don't trigger the button on 'enter' keypress
-		});
-	}
-
-	$('#container').append('<a href="/" id="backlink-bottom" class="backlink">Back</a>');
-	$('#loading').remove();
+		$('#container').append('<a href="/" id="backlink-bottom" class="backlink">Back</a>');
+		$('#loading').remove();
+	});
 });
