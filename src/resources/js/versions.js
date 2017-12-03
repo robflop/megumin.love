ebb3d21ff00f7147de98f598eb1bf68f28d034a7
robflop <robflop98@gmail.com>
2017-12-03 11:43:26 +0100

    Two more backgrounds

--- src/resources/js/versions.js
+++ src/resources/js/versions.js
@@ -3,10 +3,11 @@ $(document).ready(() => {
 
 	const backgroundCookie = (document.cookie.split(';').find(cookie => cookie.trim().startsWith('background')) || '').trim();
 	const background = backgroundCookie.substr(backgroundCookie.indexOf('=') + 1);
+	const randomBg = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg', 'bg5.jpg', 'bg6.jpg'][Math.round(Math.random() * 5)];
 
 	if (background === 'rotate' || !background) {
 		// falsy check for if no selection has been made, and thus is on standard
-		$('body').css('background-image', `url(/images/${['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg'][Math.round(Math.random() * 3)]})`);
+		$('body').css('background-image', `url(/images/${randomBg})`);
 	}
 	else {
 		$('body').css('background-image', `url(/images/${background}.jpg)`);
