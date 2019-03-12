4f99082603eabcff539a540d6a96bbb8b414ac9e
robflop <robflop98@gmail.com>
2019-03-12 17:59:00 +0100

    Generalized response element

--- src/resources/js/admin.js
+++ src/resources/js/admin.js
@@ -13,8 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	fetch('/api/sounds').then(res => res.json()).then(s => {
 		updateSounds(s);
 	}).then(() => {
+		const response = document.getElementById('response');
+
 		const uploadForm = document.getElementById('upload-form');
-		const uploadRes = document.getElementById('upload-res');
 		uploadForm.addEventListener('submit', e => {
 			e.preventDefault();
 
@@ -31,21 +32,20 @@ document.addEventListener('DOMContentLoaded', () => {
 				if (res.code === 200) {
 					uploadForm.reset();
 
-					uploadRes.innerText = 'Sound successfully uploaded!';
-					util.fade(uploadRes, 5000);
+					response.innerText = 'Sound successfully uploaded!';
+					util.fade(response, 5000);
 
 					sounds.push(res.sound);
 					return updateSounds(sounds);
 				}
 				else {
-					uploadRes.innerText = `An Error occurred (Code ${res.code}): ${res.message}`;
-					return util.fade(uploadRes, 5000);
+					response.innerText = `An Error occurred (Code ${res.code}): ${res.message}`;
+					return util.fade(response, 5000);
 				}
 			});
 		});
 
 		const renameForm = document.getElementById('rename-form');
-		const renameRes = document.getElementById('rename-res');
 		document.getElementById('rename-form').addEventListener('submit', e => {
 			e.preventDefault();
 
@@ -67,22 +67,21 @@ document.addEventListener('DOMContentLoaded', () => {
 				if (res.code === 200) {
 					renameForm.reset();
 
-					renameRes.innerText = 'Sound successfully renamed!';
-					util.fade(renameRes, 5000);
+					response.innerText = 'Sound successfully renamed!';
+					util.fade(response, 5000);
 
 					sounds[sounds.findIndex(snd => snd.id === res.sound.id)] = res.sound;
 					// Compare IDs because all other fields may have changed, id the only constant
 					return updateSounds(sounds);
 				}
 				else {
-					renameRes.innerText = `An Error occurred (Code ${res.code}): ${res.message}`;
-					return util.fade(renameRes, 5000);
+					response.innerText = `An Error occurred (Code ${res.code}): ${res.message}`;
+					return util.fade(response, 5000);
 				}
 			});
 		});
 
 		const deleteForm = document.getElementById('delete-form');
-		const deleteRes = document.getElementById('delete-res');
 		document.getElementById('delete-form').addEventListener('submit', e => {
 			e.preventDefault();
 
@@ -101,16 +100,16 @@ document.addEventListener('DOMContentLoaded', () => {
 				if (res.code === 200) {
 					deleteForm.reset();
 
-					deleteRes.innerText = 'Sound successfully deleted!';
-					util.fade(deleteRes, 5000);
+					response.innerText = 'Sound successfully deleted!';
+					util.fade(response, 5000);
 
 					sounds.splice(sounds.findIndex(snd => snd.id === res.sound.id), 1);
 					// Compare IDs because all other fields may have changed, id the only constant
 					return updateSounds(sounds);
 				}
 				else {
-					deleteRes.innerText = `An Error occurred (Code ${res.code}): ${res.message}`;
-					return util.fade(deleteRes, 5000);
+					response.innerText = `An Error occurred (Code ${res.code}): ${res.message}`;
+					return util.fade(response, 5000);
 				}
 			});
 		});
