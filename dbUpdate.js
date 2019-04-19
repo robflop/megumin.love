434bf444498de9aad945cdfdb03787757fda503e
robflop <robflop98@gmail.com>
2019-04-19 09:02:59 +0200

    Move codeblocks around

--- dbUpdate.js
+++ dbUpdate.js
@@ -7,31 +7,33 @@ const { databasePath } = require('./src/config.json');
 const db = new Database(join('src', databasePath));
 let dbVersion, newVersion;
 
-const databaseVersions = [
-	{
-		targetVersion: '6.0.0',
-		queries: [
-			'ALTER TABLE yamero_counter RENAME TO main_counter;',
-		]
-	},
-	{
-		targetVersion: '7.0.2',
-		queries: [
-			'UPDATE sounds SET filename = "objection", displayname = "Objection!!" WHERE filename = "igiari";',
-		],
-		notes: [
-			'Rename \'igiari.mp3\' in the sounds folder to \'objection.mp3\' or the sound will not work on-site anymore.'
-		]
-	},
-	{
-		targetVersion: '8.0.0',
-		queries: [
-			'ALTER TABLE sounds ADD COLUMN association TEXT;',
-			'CREATE TABLE IF NOT EXISTS meta ( dbVersion TEXT NOT NULL );',
-			'INSERT INTO meta ( dbVersion ) VALUES ( \'8.0.0\' );'
-		]
-	},
-];
+db.get('SELECT dbVersion FROM meta', [], (selectErr, data) => {
+	if (selectErr || !data || (data && !data.dbVersion)) dbVersion = 'Unknown';
+	else dbVersion = data.dbVersion;
+
+	console.log('This is the megumin.love database migration tool.');
+	console.log(`Detected database version: ${dbVersion}`);
+
+	if (dbVersion === 'Unknown') {
+		console.log('Please enter your version of megumin.love before the update (e.g. 6.0.2 or 7.1.0):');
+
+		const dbVersionInput = createInterface({
+			input: process.stdin,
+			output: process.stdout,
+			prompt: 'Starting database version: '
+		});
+
+		dbVersionInput.prompt();
+
+		dbVersionInput.on('line', ver => {
+			dbVersion = ver.trim();
+			dbVersionInput.close();
+		});
+
+		dbVersionInput.on('close', () => targetDatabaseUpdate());
+	}
+	else targetDatabaseUpdate();
+});
 
 function targetDatabaseUpdate() {
 	const migrationVersionInput = createInterface({
@@ -77,30 +79,28 @@ function targetDatabaseUpdate() {
 	});
 }
 
-db.get('SELECT dbVersion FROM meta', [], (selectErr, data) => {
-	if (selectErr || !data || (data && !data.dbVersion)) dbVersion = 'Unknown';
-	else dbVersion = data.dbVersion;
-
-	console.log('This is the megumin.love database migration tool.');
-	console.log(`Detected database version: ${dbVersion}`);
-
-	if (dbVersion === 'Unknown') {
-		console.log('Please enter your version of megumin.love before the update (e.g. 6.0.2 or 7.1.0):');
-
-		const dbVersionInput = createInterface({
-			input: process.stdin,
-			output: process.stdout,
-			prompt: 'Starting database version: '
-		});
-
-		dbVersionInput.prompt();
-
-		dbVersionInput.on('line', ver => {
-			dbVersion = ver.trim();
-			dbVersionInput.close();
-		});
-
-		dbVersionInput.on('close', () => targetDatabaseUpdate());
-	}
-	else targetDatabaseUpdate();
-});
+const databaseVersions = [
+	{
+		targetVersion: '6.0.0',
+		queries: [
+			'ALTER TABLE yamero_counter RENAME TO main_counter;',
+		]
+	},
+	{
+		targetVersion: '7.0.2',
+		queries: [
+			'UPDATE sounds SET filename = "objection", displayname = "Objection!!" WHERE filename = "igiari";',
+		],
+		notes: [
+			'Rename \'igiari.mp3\' in the sounds folder to \'objection.mp3\' or the sound will not work on-site anymore.'
+		]
+	},
+	{
+		targetVersion: '8.0.0',
+		queries: [
+			'ALTER TABLE sounds ADD COLUMN association TEXT;',
+			'CREATE TABLE IF NOT EXISTS meta ( dbVersion TEXT NOT NULL );',
+			'INSERT INTO meta ( dbVersion ) VALUES ( \'8.0.0\' );'
+		]
+	},
+];
